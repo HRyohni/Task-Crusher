6e19308eef8cb286aac5dd9d94ff4c53ceaeb138
Yohni <62948056+HRyohni@users.noreply.github.com>
2026-03-23 18:03:42 +0100

    feat(03-02): add deep link support to MainActivity
    - Define ACTION_VIEW_TASKS constant
    - Handle intent in onCreate with logging

--- app/src/main/java/hr/fipu/organizationtool/MainActivity.kt
+++ app/src/main/java/hr/fipu/organizationtool/MainActivity.kt
@@ -1,14 +1,24 @@
 package hr.fipu.organizationtool
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import hr.fipu.organizationtool.ui.FoundationLab
 import hr.fipu.organizationtool.ui.theme.ZenTheme
 
 class MainActivity : ComponentActivity() {
+    companion object {
+        const val ACTION_VIEW_TASKS = "hr.fipu.organizationtool.ACTION_VIEW_TASKS"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (intent?.action == ACTION_VIEW_TASKS) {
+            Log.d("ZenStack", "Deep link detected: ACTION_VIEW_TASKS")
+        }
+
         setContent {
             ZenTheme {
                 FoundationLab()
